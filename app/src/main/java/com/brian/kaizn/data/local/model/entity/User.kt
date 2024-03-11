3debfc8cf9b6caa32a68fc83ce0ff179b1996a7e
bryanares <bryanares12@gmail.com>
2024-03-11 21:24:53 +0300

    user entity

--- app/src/main/java/com/brian/kaizn/data/local/model/entity/User.kt
+++ app/src/main/java/com/brian/kaizn/data/local/model/entity/User.kt
@@ -1,8 +1,13 @@
 package com.brian.kaizn.data.local.model.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
-data class user (
-
+data class User (
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    var userName: Long? = null,
+    var userFirstName: Long? = null,
+    var userLastName: Long? = null,
+    var userEmail: Long? = null
 )
