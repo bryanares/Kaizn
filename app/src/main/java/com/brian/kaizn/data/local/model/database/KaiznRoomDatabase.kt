b20c9b4c716642df14429950a66649d94f5f0d4f
bryanares <bryanares12@gmail.com>
2024-03-17 15:20:36 +0300

    database class

--- app/src/main/java/com/brian/kaizn/data/local/model/database/KaiznRoomDatabase.kt
+++ app/src/main/java/com/brian/kaizn/data/local/model/database/KaiznRoomDatabase.kt
@@ -1,4 +1,17 @@
 package com.brian.kaizn.data.local.model.database
 
-class KaiznRoomDatabase {
+import androidx.room.Database
+import com.brian.kaizn.data.local.dao.KaiznDao
+import com.brian.kaizn.data.local.model.entity.HabitEntity
+import com.brian.kaizn.data.local.model.entity.HabitGoalEntity
+import com.brian.kaizn.data.local.model.entity.UserEntity
+
+
+@Database(
+    entities = [HabitEntity::class, HabitGoalEntity::class, UserEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class KaiznRoomDatabase {
+    abstract fun KaiznDao() : KaiznDao
 }
