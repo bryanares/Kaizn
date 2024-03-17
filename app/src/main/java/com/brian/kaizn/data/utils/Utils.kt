90e697da07d52dd7607545fdabe9487b506e20e0
bryanares <bryanares12@gmail.com>
2024-03-17 15:21:42 +0300

    utils

--- app/src/main/java/com/brian/kaizn/data/utils/Utils.kt
+++ app/src/main/java/com/brian/kaizn/data/utils/Utils.kt
@@ -1,4 +1,8 @@
 package com.brian.kaizn.data.utils
 
-class Utils {
+sealed class Rezults<T> {
+    data class Success<T>(val data: T) : Rezults<T>()
+    data class Error<T>(val message: String? = null, val exception: Exception? = null) :
+        Rezults<T>()
+
 }
