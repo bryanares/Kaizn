c1cfc185dc490de4365de9eddcad5a0e2628ad48
bryanares <bryanares12@gmail.com>
2024-03-17 15:21:19 +0300

    define and implement repository

--- app/src/main/java/com/brian/kaizn/data/repository/KaiznRepository.kt
+++ app/src/main/java/com/brian/kaizn/data/repository/KaiznRepository.kt
@@ -1,4 +1,24 @@
 package com.brian.kaizn.data.repository
 
+import com.brian.kaizn.data.local.model.entity.HabitEntity
+import com.brian.kaizn.data.utils.Rezults
+import kotlinx.coroutines.flow.Flow
+
 interface KaiznRepository {
+
+    //create new habit
+    ///what do I expect as return? Flow<sealed class>
+    suspend fun createNewHabit(newHabit : HabitEntity): Flow<Rezults<HabitEntity>>
+
+    //update
+    suspend fun updateExistingHabit (habitId: Long?, existingHabit : HabitEntity): Flow<Rezults<HabitEntity>>
+
+    //delete habit
+    suspend fun deleteSingleHabit(habitId: Long, userId: Long): Flow<Rezults<List<HabitEntity>>>
+
+    //get single habit
+    suspend fun getSingleHabit(habitId: Long): Flow<Rezults<HabitEntity>>
+
+    //get list of habits
+    suspend fun getAllHabits(userId: Long): Flow<Rezults<List<HabitEntity>>>
 }
--- app/src/main/java/com/brian/kaizn/data/repository/KaiznRepositoryImplementation.kt
+++ app/src/main/java/com/brian/kaizn/data/repository/KaiznRepositoryImplementation.kt
@@ -1,4 +1,33 @@
 package com.brian.kaizn.data.repository
 
-class KaiznRepositoryImplementation {
+import com.brian.kaizn.data.local.model.entity.HabitEntity
+import com.brian.kaizn.data.utils.Rezults
+import kotlinx.coroutines.flow.Flow
+
+class KaiznRepositoryImplementation(): KaiznRepository {
+    override suspend fun createNewHabit(newHabit: HabitEntity): Flow<Rezults<HabitEntity>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateExistingHabit(
+        habitId: Long?,
+        existingHabit: HabitEntity
+    ): Flow<Rezults<HabitEntity>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteSingleHabit(
+        habitId: Long,
+        userId: Long
+    ): Flow<Rezults<List<HabitEntity>>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getSingleHabit(habitId: Long): Flow<Rezults<HabitEntity>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllHabits(userId: Long): Flow<Rezults<List<HabitEntity>>> {
+        TODO("Not yet implemented")
+    }
 }
