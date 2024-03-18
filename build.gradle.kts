59e0116c5e410df071159ca80ae5bb4628131a91
bryanares <bryanares12@gmail.com>
2024-03-18 11:17:51 +0300

    dagger-hilt dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,6 @@ plugins {
 //    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
     alias(libs.plugins.androidKsp) apply false
 //    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
 
 }
