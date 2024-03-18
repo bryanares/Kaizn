a714ff7b465db663893f36faeb9abd3e21902057
bryanares <bryanares12@gmail.com>
2024-03-18 11:14:29 +0300

    embedded class with relationship

--- app/src/main/java/com/brian/kaizn/data/local/model/entity/HabitWithGoalEntity.kt
+++ app/src/main/java/com/brian/kaizn/data/local/model/entity/HabitWithGoalEntity.kt
@@ -1,4 +1,20 @@
 package com.brian.kaizn.data.local.model.entity
 
-class HabitWithGoalEntity {
-}
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.Relation
+
+//habit with goal
+/**query HabitEntity first, get habitId then query HabitGoalEntity and search for corresponding value
+under the foreign key column name habitEntityId
+ */
+
+@Entity
+data class HabitWithGoalEntity(
+    @Embedded val habitEntity: HabitEntity,
+    @Relation(
+        parentColumn = "habitId",
+        entityColumn = "habitEntityId"
+    )
+    val habitGoalEntity: HabitGoalEntity
+)
