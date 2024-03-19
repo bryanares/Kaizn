1f0c7815813df9f68ea9619115afb99ed1bf4cc0
bryanares <bryanares12@gmail.com>
2024-03-19 11:25:20 +0300

    android entry point

--- app/src/main/java/com/brian/kaizn/MainActivity.kt
+++ app/src/main/java/com/brian/kaizn/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.brian.kaizn.ui.theme.KaiznTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
