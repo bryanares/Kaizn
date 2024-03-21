66007841fec8375aaf9b9edcd8bbf1949262093d
bryanares <bryanares12@gmail.com>
2024-03-21 13:51:48 +0300

    update return types

--- app/src/main/java/com/brian/kaizn/data/local/dao/KaiznDao.kt
+++ app/src/main/java/com/brian/kaizn/data/local/dao/KaiznDao.kt
@@ -8,21 +8,26 @@ import androidx.room.Transaction
 import androidx.room.Update
 import com.brian.kaizn.data.local.model.entity.HabitEntity
 import com.brian.kaizn.data.local.model.entity.HabitWithGoalEntity
+import com.brian.kaizn.data.utils.Rezults
 
 @Dao
 interface KaiznDao {
 
     //create new habit
+    //edited return to HabitWithGoalEntity
     @Insert
-    suspend fun insertNewHabit(newHabit: HabitEntity)
+    suspend fun insertNewHabit(newHabit: HabitWithGoalEntity): List<HabitWithGoalEntity>
 
     //update existing habit
     @Update
-    suspend fun updateExistingHabit(existingHabit: HabitEntity)
+    suspend fun updateExistingHabit(
+        habitId: Long,
+        existingHabit: HabitWithGoalEntity
+    ): List<HabitWithGoalEntity>
 
     //delete existing habit
     @Delete
-    suspend fun delete(habit: HabitEntity)
+    suspend fun deleteHabit(habitId: Long, habit: HabitWithGoalEntity): List<HabitWithGoalEntity>
 
     //delete multiple habits
     @Delete
