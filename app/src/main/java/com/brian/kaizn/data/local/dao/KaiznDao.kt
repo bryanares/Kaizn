21856ea35bfd41e0300b6d2088144d83484bc8a6
bryanares <bryanares12@gmail.com>
2024-03-21 21:20:44 +0300

    edit dao methods

--- app/src/main/java/com/brian/kaizn/data/local/dao/KaiznDao.kt
+++ app/src/main/java/com/brian/kaizn/data/local/dao/KaiznDao.kt
@@ -8,7 +8,7 @@ import androidx.room.Transaction
 import androidx.room.Update
 import com.brian.kaizn.data.local.model.entity.HabitEntity
 import com.brian.kaizn.data.local.model.entity.HabitWithGoalEntity
-import com.brian.kaizn.data.utils.Rezults
+import com.brian.kaizn.data.repository.ReturnNothing
 
 @Dao
 interface KaiznDao {
@@ -16,18 +16,17 @@ interface KaiznDao {
     //create new habit
     //edited return to HabitWithGoalEntity
     @Insert
-    suspend fun insertNewHabit(newHabit: HabitWithGoalEntity): List<HabitWithGoalEntity>
+    suspend fun insertNewHabit(newHabit: HabitEntity): Long
 
     //update existing habit
     @Update
     suspend fun updateExistingHabit(
-        habitId: Long,
         existingHabit: HabitWithGoalEntity
-    ): List<HabitWithGoalEntity>
+    )
 
     //delete existing habit
     @Delete
-    suspend fun deleteHabit(habitId: Long, habit: HabitWithGoalEntity): List<HabitWithGoalEntity>
+    suspend fun deleteHabit(habit: HabitWithGoalEntity): List<ReturnNothing>
 
     //delete multiple habits
     @Delete
@@ -47,6 +46,6 @@ interface KaiznDao {
     //is the query correct?
     @Transaction
     @Query("SELECT * FROM HabitEntity WHERE habitId = :habitEntityId")
-    suspend fun getHabitWithGoal(habitEntityId: Long): List<HabitWithGoalEntity>? = null
+    suspend fun getHabitWithGoal(habitEntityId: Long): HabitWithGoalEntity
 
 }
