3036206f79010539137f197aff2ef9e91c279468
bryanares <bryanares12@gmail.com>
2024-03-22 19:11:37 +0300

    mock repository

--- app/src/test/java/com/brian/kaizn/repository/MockKaiznRepositoryImplementation.kt
+++ app/src/test/java/com/brian/kaizn/repository/MockKaiznRepositoryImplementation.kt
@@ -1,4 +1,30 @@
 package com.brian.kaizn.repository
 
-class MockKaiznRepositoryImplementation {
+import com.brian.kaizn.data.local.model.entity.HabitEntity
+import com.brian.kaizn.data.local.model.entity.HabitGoalEntity
+import com.brian.kaizn.data.local.model.entity.HabitWithGoalEntity
+import com.brian.kaizn.data.repository.KaiznRepository
+import com.brian.kaizn.data.repository.ReturnNothing
+import com.brian.kaizn.data.utils.Rezults
+
+class MockKaiznRepositoryImplementation : KaiznRepository {
+    override suspend fun createNewHabit(newHabit: HabitEntity): Rezults<Long> {
+        return Rezults.Success(777)
+    }
+
+    override suspend fun updateExistingHabit(existingHabit: HabitWithGoalEntity): Rezults<ReturnNothing> {
+        return Rezults.Success(ReturnNothing())
+    }
+
+    override suspend fun deleteSingleHabit(habit: HabitWithGoalEntity): Rezults<ReturnNothing> {
+        return Rezults.Success(ReturnNothing())
+    }
+
+    override suspend fun getSingleHabit(habitId: Long): Rezults<HabitWithGoalEntity> {
+        return Rezults.Success(HabitWithGoalEntity(HabitEntity(), HabitGoalEntity()))
+    }
+
+    override suspend fun getAllHabits(): Rezults<List<HabitWithGoalEntity>> {
+        return Rezults.Success(listOf(HabitWithGoalEntity(HabitEntity(), HabitGoalEntity())))
+    }
 }
