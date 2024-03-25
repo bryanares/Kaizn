b4dc6960c327aded123b79a6c1770c040c064740
bryanares <bryanares12@gmail.com>
2024-03-25 20:41:19 +0300

    implement getSingleHabit

--- app/src/main/java/com/brian/kaizn/features/habit_history/domain/viewmodel/KaiznViewModel.kt
+++ app/src/main/java/com/brian/kaizn/features/habit_history/domain/viewmodel/KaiznViewModel.kt
@@ -1,14 +1,38 @@
 package com.brian.kaizn.features.habit_history.domain.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.brian.kaizn.data.repository.KaiznRepositoryImplementation
+import androidx.lifecycle.viewModelScope
+import com.brian.kaizn.data.di.DefaultDispatcher
+import com.brian.kaizn.data.di.IoDispatcher
+import com.brian.kaizn.data.di.MainDispatcher
+import com.brian.kaizn.data.repository.KaiznRepository
+import com.brian.kaizn.data.utils.Rezults
+import com.brian.kaizn.features.habit_history.domain.model.KaiznUiEvents
+import com.brian.kaizn.features.habit_history.domain.model.KaiznUiEvents.Error
+import com.brian.kaizn.features.habit_history.domain.model.KaiznUiStates
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class KaiznViewModel @Inject constructor(
-    private val kaiznRepository: KaiznRepositoryImplementation
+    private val kaiznRepository: KaiznRepository,
+    @IoDispatcher private val ioDispatcher : CoroutineDispatcher,
+    @DefaultDispatcher private val defaultDispatcher : CoroutineDispatcher,
+    @MainDispatcher private val mainDispatcher : CoroutineDispatcher
 ) : ViewModel() {
+
+    private val _kaiznUiState = MutableStateFlow(KaiznUiStates())
+    val kaiznUiState = _kaiznUiState.asStateFlow()
+
+    private val _kaiznUiEvent = Channel<KaiznUiEvents>()
+    val kaiznUiEvent = _kaiznUiEvent.receiveAsFlow()
 
 
     fun createHabit() {
@@ -23,7 +47,32 @@ class KaiznViewModel @Inject constructor(
 
     }
 
-    fun getSingleHabit() {
+    fun getSingleHabit(habitId: Long) {
+        viewModelScope.launch(ioDispatcher) {
+            _kaiznUiState.update {
+                it.copy(isLoading = true)
+            }
+            val habit = kaiznRepository.getSingleHabit(habitId)
+            when(habit){
+                is Rezults.Success -> {
+                    _kaiznUiState.update {
+                        it.copy(
+                            isLoading = false,
+                            isSuccessful = true,
+                            singleSelectedHabit = habit.data
+                        )
+                    }
+                }
+                is Rezults.Error ->{
+                    //
+                    _kaiznUiEvent.send(KaiznUiEvents.Error(message = habit.message))
+                }
+            }
+            _kaiznUiState.update {
+                it.copy(isLoading = false)
+            }
+
+        }
 
     }
 
