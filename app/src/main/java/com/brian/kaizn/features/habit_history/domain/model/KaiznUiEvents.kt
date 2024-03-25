2a5bdccde3ddf5f31ba25a6e1fcc0c567b34305b
bryanares <bryanares12@gmail.com>
2024-03-25 20:40:54 +0300

    create ui and event state models

--- app/src/main/java/com/brian/kaizn/features/habit_history/domain/model/KaiznUiEvents.kt
+++ app/src/main/java/com/brian/kaizn/features/habit_history/domain/model/KaiznUiEvents.kt
@@ -1,4 +1,11 @@
 package com.brian.kaizn.features.habit_history.domain.model
 
-class KaiznUiEvents {
+import com.brian.kaizn.data.utils.Rezults
+
+sealed class KaiznUiEvents{
+    //when I specify generic type <T>, I get an error when declaring this property in the viewmodel class:
+    //one type argument expected for class KaiznUiEvent<T>
+    data class Error(val message: String? = null) : KaiznUiEvents()
+    data class Success(val data: Long) : KaiznUiEvents()
+
 }
--- app/src/main/java/com/brian/kaizn/features/habit_history/domain/model/KaiznUiStates.kt
+++ app/src/main/java/com/brian/kaizn/features/habit_history/domain/model/KaiznUiStates.kt
@@ -1,4 +1,17 @@
 package com.brian.kaizn.features.habit_history.domain.model
 
-class KaiznUiStates {
-}
+import com.brian.kaizn.data.local.model.entity.HabitEntity
+import com.brian.kaizn.data.local.model.entity.HabitWithGoalEntity
+
+data class KaiznUiStates (
+    val isLoading: Boolean = false,
+    val isSuccessful: Boolean = false,
+    val error: String? = null,
+    val habitId: String? = null,
+    val createdHabit: HabitWithGoalEntity? = null,
+    val updatedHabit: HabitWithGoalEntity? = null,
+    val singleSelectedHabit: HabitWithGoalEntity? = null,
+    val habitList: List<HabitWithGoalEntity>? = null,
+    val message: String? = null
+
+)
