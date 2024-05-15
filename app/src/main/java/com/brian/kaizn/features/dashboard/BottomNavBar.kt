d5a45a11570e46aa552c815c8c03db8792b054bc
bryanares <bryanares12@gmail.com>
2024-05-15 18:50:56 +0300

    create bottom nav and navDirections

--- app/src/main/java/com/brian/kaizn/features/dashboard/BottomNavBar.kt
+++ app/src/main/java/com/brian/kaizn/features/dashboard/BottomNavBar.kt
@@ -1,2 +1,59 @@
 package com.brian.kaizn.features.dashboard
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun KaiznBottomBar(
+    navController: NavHostController
+) {
+    NavigationBar() {
+
+        NavigationBarItem(
+            selected = false,
+            onClick = { navController.navigate("home") },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = "Home"
+                )
+            }
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { navController.navigate("History") },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = "History"
+                )
+            }
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { navController.navigate("Profile") },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = "Profile"
+                )
+            }
+        )
+    }
+}
+
+@Composable
+fun currentRoute(navController: NavHostController) :String?{
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+    return currentDestination?.route
+}
+
--- app/src/main/java/com/brian/kaizn/features/dashboard/NavDashboard.kt
+++ app/src/main/java/com/brian/kaizn/features/dashboard/NavDashboard.kt
@@ -1,2 +1,51 @@
 package com.brian.kaizn.features.dashboard
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+//nav controller
+@Composable
+fun NavDashboard(modifier: Modifier = Modifier) {
+
+    val navHostController = rememberNavController()
+    Scaffold(bottomBar = {
+        KaiznBottomBar(navController = navHostController)
+    }) {
+
+        NavDashboardDestinations(
+            navHostController = navHostController,
+            modifier = Modifier.padding(it)
+        )
+    }
+
+}
+
+@Composable
+fun NavDashboardDestinations(modifier: Modifier = Modifier, navHostController: NavHostController) {
+
+    //nav controller
+    NavHost(navController = navHostController, startDestination = "Home") {
+        composable(route = "Home") {
+            Text(text = "Home")
+        }
+    }
+    NavHost(navController = navHostController, startDestination = "History") {
+        composable(route = "History") {
+
+            Text(text = "History")
+        }
+    }
+    NavHost(navController = navHostController, startDestination = "Profile") {
+        composable(route = "Profile") {
+
+            Text(text = "Profile")
+        }
+    }
+}
