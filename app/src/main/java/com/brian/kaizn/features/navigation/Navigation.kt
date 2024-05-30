3b5101e690ca0f1d11bf2dece5230411d02202c4
bryanares <bryanares12@gmail.com>
2024-05-30 17:54:32 +0300

    set up navigation host

--- app/src/main/java/com/brian/kaizn/features/navigation/Navigation.kt
+++ app/src/main/java/com/brian/kaizn/features/navigation/Navigation.kt
@@ -1,4 +1,43 @@
 package com.brian.kaizn.features.navigation
 
-class Navigation {
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.brian.kaizn.R
+import com.brian.kaizn.features.auth.presentation.login.Login
+import com.brian.kaizn.features.auth.presentation.registration.Registration
+import com.brian.kaizn.features.habit_history.presentation.homeScreen.Home
+
+
+enum class AppDestinations {
+    LOGIN,
+    REGISTRATION,
+    HOME
+}
+
+@Composable
+fun AppNavigationHost(modifier: Modifier = Modifier, navController: NavHostController) {
+
+    NavHost(navController = navController, startDestination = AppDestinations.LOGIN.name) {
+        composable(AppDestinations.LOGIN.name) {
+            Login(onRegister = {
+                navController.navigate(AppDestinations.REGISTRATION.name)
+            }, onLoggedIn = {
+                navController.navigate(AppDestinations.HOME.name)
+            })
+        }
+        composable(AppDestinations.REGISTRATION.name) {
+            Registration()
+        }
+
+        composable(AppDestinations.HOME.name) {
+            Home(
+                modifier = Modifier,
+                userName = "Brian",
+                profileImageResId = R.drawable.apex
+            )
+        }
+    }
 }
