5b3526f31027a1ddef11c4ad3cbcc83e8a46480a
bryanares <bryanares12@gmail.com>
2024-06-27 17:01:53 +0300

    ui states

--- app/src/main/java/com/brian/kaizn/features/auth/domain/model/AuthUiState.kt
+++ app/src/main/java/com/brian/kaizn/features/auth/domain/model/AuthUiState.kt
@@ -1,2 +1,8 @@
 package com.brian.kaizn.features.auth.domain.model
 
+class AuthUiState (
+    var isLoading: Boolean = false,
+    var isSuccessful: Boolean = false,
+    var error: String? = null,
+    var userId: String? = null
+)
